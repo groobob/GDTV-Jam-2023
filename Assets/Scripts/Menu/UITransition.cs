cd4fdbef2a0e9118a001551c420e14ea09963563
sevan879 <102706757+sevan879@users.noreply.github.com>
2023-05-21 16:41:08 -0400

    a

--- Assets/Scripts/Menu/UITransition.cs
+++ Assets/Scripts/Menu/UITransition.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UITransition : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    TextMeshProUGUI text;
+    public void fadeOutText()
+    {
+        LeanTween.alpha(gameObject, 0, 5f);
+    }
     void Start()
     {
-        
+        Debug.Log(gameObject);
+        //gameObject.GetComponent<TextMeshProUGUI>().alpha = 0.5f;
+        //LeanTween.color(gameObject, new Color(1,1,1,0), 1);
+        text = GetComponent<TextMeshProUGUI>();
+        var color = text.color;
+        var fadeoutcolor = color;
+        fadeoutcolor.a = 0;
+        LeanTween.value(gameObject, updateValueExampleCallback, fadeoutcolor, color, 1f).setEase(LeanTweenType.easeOutElastic).setDelay(2f);
     }
-
-    // Update is called once per frame
-    void Update()
+    void updateValueExampleCallback(Color val)
     {
-        
+        text.color = val;
     }
 }

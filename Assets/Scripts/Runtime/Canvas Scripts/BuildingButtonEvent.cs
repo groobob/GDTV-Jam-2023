3ac94a6c77329b2b3caa6fb8210c777aebbc7c23
ZacharyH777 <93843297+ZacharyH777@users.noreply.github.com>
2023-05-27 09:35:08 -0700

    SFX audio added
    Bug fixes and sfx added to audiomanager. Created an sfx list to help organize the audiomanager for sound effects. Fixed another async issue with the plots of land. Problem was a duplication glitch where one plot of land house would show up in both dimensions when bought.

--- Assets/Scripts/Runtime/Canvas Scripts/BuildingButtonEvent.cs
+++ Assets/Scripts/Runtime/Canvas Scripts/BuildingButtonEvent.cs
@@ -7,21 +7,39 @@ public class BuildingButtonEvent : MonoBehaviour
     public static event OnBuildingPurchaseEvent OnBuildingPurchaseAttempt;
     public delegate void OnBuildingPurchaseEvent(BuildingsData buildingData);
 
+
+    private GameObject AudioObject;
+    private Audio Aud;
+
+    void Awake()
+    {
+        AudioObject = GameObject.Find("AudioManager");
+        Aud = AudioObject.GetComponent<Audio>();
+    }
+
     public void PurchaseBuildingAttempt(string buildingName)
     {
         Debug.Log("Function Ran");
         if(BuildingDataSettings.Data.TryGetValue(buildingName, out BuildingsData data))
         {
-            Debug.Log("Found Building Data");
+            Debug.Log("Found Building Data " + buildingName);
             if(ResourceManager.Resources.gold >= data.buildingCost)
             {
+            if(this.gameObject.name == "Canvas2")
+                GameObject.Find("CityView1Canvas").SetActive(false);
+            if(this.gameObject.name == "Canvas")
+                GameObject.Find("CityView2Canvas").SetActive(false);
+
+
                 Debug.Log("Attempting Purchase");
-                if(this.gameObject.name == "Canvas2")
-                    GameObject.Find("CityView1Canvas").SetActive(false);
-                if(this.gameObject.name == "Canvas")
-                    GameObject.Find("CityView2Canvas").SetActive(false);
+                if(buildingName == "farm" || buildingName == "house") Aud.playWoodBuild();
+                else Aud.playMetalBuild();   
+
+
                 OnBuildingPurchaseAttempt?.Invoke(data);
             }
+
+            
         }
         else
         {

46f5ed98449e0e238c606d1b106c0beee58c613e
ZacharyH777 <93843297+ZacharyH777@users.noreply.github.com>
2023-05-29 00:29:55 -0700

    MidMerge To get all assets from everyone else
    Needs a submit and cancel option and an object option

---
+++ Assets/Scripts/Runtime/Enemy/DealWithEnemy/EnemyClciked.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClciked : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void AttemptTargetEnemy(GameObject enemy)
+    {
+        FindObjectOfType<ViewManager>().GetComponent<ViewManager>().LoadScene(GameScenes.ArmyUIScene1);
+    }
+}
